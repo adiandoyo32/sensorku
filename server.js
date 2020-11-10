889a9796faeaa7fa0df5d901ddef68b6519e6170
akhiang <akhiang32@gmail.com>
2020-11-10 19:41:43 +0700

    express

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
-const express = require('express');
-const path = require('path');
-const port = process.env.PORT || 8080;
-const app = express();
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('/ping', function (req, res) {
-  return res.send('ping');
-});
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html')); //serving build folder
-});
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+
+app = express();
+
+app.use(serveStatic(__dirname + "/dist"));
+var port = process.env.PORT || 5000;
 app.listen(port);
